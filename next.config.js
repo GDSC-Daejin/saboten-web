6ccc11703f5275889507416eb4109aaa1d6364ba
shinequasar <shinequasar@gmail.com>
2023-02-16 15:29:51 +0900

    [feat] config baseURL

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
     removeConsole: process.env.NODE_ENV === "production"
-  }
+  },
+  basePath: '/'
 }
 
 module.exports = nextConfig
